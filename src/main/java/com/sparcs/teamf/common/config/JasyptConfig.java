52722d7e4c10fd49a2da34c53ef98716987897a1
서명현 <9812smh@naver.com>
2023-01-27 20:48:44 +0900

    feat: 프로젝트 초기 세팅 (#8)
    * feat: jasypt로 프로퍼티 암호화
    
    issue: #5
    
    * feat: properties 분리 및 rds 세팅
    
    issue: #5

---
+++ src/main/java/com/sparcs/teamf/common/config/JasyptConfig.java
@@ -0,0 +1,31 @@
+package com.sparcs.teamf.common.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = getSimpleStringPBEConfig();
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+    private SimpleStringPBEConfig getSimpleStringPBEConfig() {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setPoolSize("1");
+        return config;
+    }
+}
