6c7a359ee894ae7ba7f6ed30b43b7446d5fc09c5
sa46lll <azure394@gmail.com>
2023-01-27 19:53:23 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/sparcs/teamf/TeamfApplication.java
@@ -0,0 +1,13 @@
+package com.sparcs.teamf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparcs/teamf/TeamfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparcs.teamf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
