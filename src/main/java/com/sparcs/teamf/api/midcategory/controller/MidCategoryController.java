145387e176a6000f50cacc348081f96bcb7ad82e
송은우 <80899085+be-student@users.noreply.github.com>
2023-01-28 13:15:07 +0900

    feat: midCategory 조회 기능 추가 (#23)

--- src/main/java/com/sparcs/teamf/api/midcategory/controller/MidCategoryController.java
+++ src/main/java/com/sparcs/teamf/api/midcategory/controller/MidCategoryController.java
@@ -1,9 +1,11 @@
 package com.sparcs.teamf.api.midcategory.controller;
 
 import com.sparcs.teamf.api.midcategory.dto.MidCategoriesResponse;
+import com.sparcs.teamf.api.midcategory.dto.MidCategoryResponse;
 import com.sparcs.teamf.api.midcategory.service.MidCategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,4 +22,8 @@ public class MidCategoryController {
         return midCategoryService.getByMainCategory(mainCategoryId);
     }
 
+    @GetMapping("/{midCategoryId}")
+    public MidCategoryResponse getMidCategories(@PathVariable long midCategoryId) {
+        return midCategoryService.getByMidCategoryId(midCategoryId);
+    }
 }
--- src/main/java/com/sparcs/teamf/api/midcategory/service/MidCategoryService.java
+++ src/main/java/com/sparcs/teamf/api/midcategory/service/MidCategoryService.java
@@ -3,8 +3,11 @@ package com.sparcs.teamf.api.midcategory.service;
 import com.sparcs.teamf.api.maincategory.exception.MainCategoryNotFoundException;
 import com.sparcs.teamf.api.midcategory.dto.MidCategoriesResponse;
 import com.sparcs.teamf.api.midcategory.dto.MidCategoryResponse;
+import com.sparcs.teamf.api.midcategory.exception.MidCategoryNotFoundException;
 import com.sparcs.teamf.domain.maincategory.MainCategory;
 import com.sparcs.teamf.domain.maincategory.MainCategoryRepository;
+import com.sparcs.teamf.domain.midcategory.MidCategory;
+import com.sparcs.teamf.domain.midcategory.MidCategoryRepository;
 import java.util.List;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class MidCategoryService {
 
     private final MainCategoryRepository mainCategoryRepository;
+    private final MidCategoryRepository midCategoryRepository;
 
     @Transactional
     public MidCategoriesResponse getByMainCategory(Long mainCategoryId) {
@@ -22,10 +26,17 @@ public class MidCategoryService {
                 .orElseThrow(MainCategoryNotFoundException::new);
 
         List<MidCategoryResponse> midCategoryResponses = mainCategory.getMidCategories().stream()
-                .map(midCategory -> new MidCategoryResponse(midCategory.getId(),midCategory.getName(),
+                .map(midCategory -> new MidCategoryResponse(midCategory.getId(), midCategory.getName(),
                         midCategory.getImageUrl()))
                 .toList();
 
         return new MidCategoriesResponse(mainCategory.getName(), midCategoryResponses);
+    }
+
+    @Transactional
+    public MidCategoryResponse getByMidCategoryId(long midCategoryId) {
+        MidCategory midCategory = midCategoryRepository.findById(midCategoryId)
+                .orElseThrow(MidCategoryNotFoundException::new);
+        return new MidCategoryResponse(midCategory.getId(), midCategory.getName(), midCategory.getImageUrl());
     }
 }
