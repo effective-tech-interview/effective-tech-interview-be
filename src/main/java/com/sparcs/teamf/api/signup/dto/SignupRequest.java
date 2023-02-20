c8ec8aaac2efe0f999290aaa231d06d5bd606e64
sa46lll <9812smh@naver.com>
2023-02-20 12:43:26 +0900

    fix: validation 수정

--- src/main/java/com/sparcs/teamf/api/signup/dto/SignupRequest.java
+++ src/main/java/com/sparcs/teamf/api/signup/dto/SignupRequest.java
@@ -11,7 +11,7 @@ public record SignupRequest(
         String email,
 
         @NotBlank(message = "올바른 비밀번호를 입력해주세요.")
-        @Length(min = 8, max = 30, message = "비밀번호는 영문, 숫자 조합 8자 이상 20자 이하입니다. ")
+        @Length(min = 8, max = 20, message = "비밀번호는 영문, 숫자 조합 8자 이상 20자 이하입니다. ")
         @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "비밀번호는 영문, 숫자 조합 8자 이상 20자 이하입니다.")
         String password,
 
