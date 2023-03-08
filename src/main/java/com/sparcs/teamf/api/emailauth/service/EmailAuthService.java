cd7b2d83bff06439b083c4b4bd877a0e9f384930
서명현 <azure394@gmail.com>
2023-03-08 12:25:43 +0900

    feat: 비밀번호 재설정 기능 구현 (#22)
    * feat: 이메일 발송 유형 event 컬럼 추가
    
    issue: #21
    
    * feat: 회원 가입 및 이메일 발송 시 event 컬럼 적용
    
    issue: #21
    
    * fix: api response 수정
    
    * feat: 비밀번호 재설정 이메일 인증 코드 요청 기능 구현
    
    issue: #21
    
    * refactor: 비밀번호 재설정 auth 도메인으로 위치 변경
    
    issue: #21
    
    * feat: 비밀번호 재설정 이메일 인증 코드 검증 기능 구현
    
    issue: #21
    
    * feat: 비밀번호 재설정 기능 구현
    
    issue: #21
    
    * feat: 이메일 인증 시 일회용 토큰 발급
    
    issue: #21
    
    * refactor: 비밀번호 재설정 도메인 변경
    
    * feat: swagger api response 추가
    
    * fix: transaction 어노테이션 추가
    
    * refactor: swagger summary 수정

--- src/main/java/com/sparcs/teamf/api/emailauth/service/EmailAuthService.java
+++ src/main/java/com/sparcs/teamf/api/emailauth/service/EmailAuthService.java
@@ -1,12 +1,20 @@
 package com.sparcs.teamf.api.emailauth.service;
 
+import static com.sparcs.teamf.domain.emailauth.Event.REGISTRATION;
+import static com.sparcs.teamf.domain.emailauth.Event.RESET_PASSWORD;
+
+import com.sparcs.teamf.api.auth.dto.OneTimeTokenResponse;
+import com.sparcs.teamf.api.auth.jwt.TokenProvider;
 import com.sparcs.teamf.api.emailauth.exception.EmailRequestRequiredException;
 import com.sparcs.teamf.api.emailauth.exception.VerificationCodeMismatchException;
 import com.sparcs.teamf.api.member.exception.DuplicateEmailException;
+import com.sparcs.teamf.api.member.exception.MemberNotFoundException;
 import com.sparcs.teamf.domain.emailauth.EmailAuth;
 import com.sparcs.teamf.domain.emailauth.EmailAuthRepository;
+import com.sparcs.teamf.domain.member.Member;
 import com.sparcs.teamf.domain.member.MemberRepository;
 import java.util.concurrent.ThreadLocalRandom;
+import javax.annotation.Resource;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,31 +24,55 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailAuthService {
 
-    private final EmailService emailService;
+    @Resource(name = "signupEmailService")
+    private final EmailService signupEmailService;
+
+    @Resource(name = "resetPasswordEmailService")
+    private final EmailService resetPasswordEmailService;
+
     private final EmailAuthRepository emailAuthRepository;
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
     private final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     public void sendEmailForSignup(String email) {
-        validateAlreadyRegistered(email);
-
+        if (isAlreadyRegistered(email)) {
+            throw new DuplicateEmailException();
+        }
         int verificationCode = generateVerificationCode();
-        emailService.send(email, verificationCode);
-        emailAuthRepository.save(EmailAuth.of(email, verificationCode));
+        signupEmailService.send(email, verificationCode);
+        emailAuthRepository.save(EmailAuth.of(email, REGISTRATION, verificationCode));
     }
 
     public void authenticateEmailForSignup(String email, int inputVerificationCode) {
-        EmailAuth emailAuth = emailAuthRepository.findFirstByEmailOrderByCreatedDateDesc(email)
+        EmailAuth emailAuth = emailAuthRepository.findFirstByEmailAndEventOrderByCreatedDateDesc(email, REGISTRATION)
                 .orElseThrow(EmailRequestRequiredException::new);
 
         verifyVerificationCodeMismatch(emailAuth.getVerificationCode(), inputVerificationCode);
         emailAuth.authenticate();
     }
 
-    private void validateAlreadyRegistered(String email) {
-        if (memberRepository.existsByEmail(email)) {
-            throw new DuplicateEmailException();
+    public void sendPasswordResetCode(String email) {
+        if (!isAlreadyRegistered(email)) {
+            throw new MemberNotFoundException();
         }
+        int verificationCode = generateVerificationCode();
+        resetPasswordEmailService.send(email, verificationCode);
+        emailAuthRepository.save(EmailAuth.of(email, RESET_PASSWORD, verificationCode));
+    }
+
+    public OneTimeTokenResponse verifyPasswordResetCode(String email, Integer inputVerificationCode) {
+        EmailAuth emailAuth = emailAuthRepository.findFirstByEmailAndEventOrderByCreatedDateDesc(email, RESET_PASSWORD)
+                .orElseThrow(EmailRequestRequiredException::new);
+        Member member = memberRepository.findByEmail(email).orElseThrow(MemberNotFoundException::new);
+
+        verifyVerificationCodeMismatch(emailAuth.getVerificationCode(), inputVerificationCode);
+        emailAuth.authenticate();
+        return tokenProvider.createOneTimeToken(member.getId(), email);
+    }
+
+    private boolean isAlreadyRegistered(String email) {
+        return memberRepository.existsByEmail(email);
     }
 
     private int generateVerificationCode() {
