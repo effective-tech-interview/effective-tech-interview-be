89765bc9120cfb68502c98a425c3df1f276351f8
Be-student <presentsong@naver.com>
2023-03-08 23:15:20 +0900

    fix: embedded redis 제거

--- src/test/java/com/sparcs/teamf/TeamfApplicationTests.java
+++ src/test/java/com/sparcs/teamf/TeamfApplicationTests.java
@@ -2,12 +2,14 @@ package com.sparcs.teamf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TeamfApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
