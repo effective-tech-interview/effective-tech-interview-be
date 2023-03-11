ec291875cb5341d19ffeea023d461623c26b14d9
Be-student <presentsong@naver.com>
2023-03-11 11:49:11 +0900

    feat: springdoc security 추가로 hide 제거

--- src/main/java/com/sparcs/teamf/api/auth/dto/EffectiveMember.java
+++ src/main/java/com/sparcs/teamf/api/auth/dto/EffectiveMember.java
@@ -1,13 +1,11 @@
 package com.sparcs.teamf.api.auth.dto;
 
+import java.util.Collection;
+import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.List;
 
-
-@io.swagger.v3.oas.annotations.Hidden
 public class EffectiveMember implements UserDetails {
 
     private final Long memberId;
