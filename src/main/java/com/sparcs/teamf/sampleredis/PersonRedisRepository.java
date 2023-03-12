789184cadb15d5c45a146f77d5d0d9c86b6bd1e7
Be-student <presentsong@naver.com>
2023-03-12 21:53:58 +0900

    chore: 샘플 제거

--- src/main/java/com/sparcs/teamf/sampleredis/PersonRedisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparcs.teamf.sampleredis;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRedisRepository extends CrudRepository<Person, String> {
-
-}
