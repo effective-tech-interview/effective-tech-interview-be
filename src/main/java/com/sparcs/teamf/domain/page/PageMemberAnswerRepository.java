0e765fe636d0684b559ded97112fca65e210435e
sa46lll <azure394@gmail.com>
2023-03-15 17:54:44 +0900

    feat: 추가된 엔티티 repository 생성
    issue: #35

---
+++ src/main/java/com/sparcs/teamf/domain/page/PageMemberAnswerRepository.java
@@ -0,0 +1,7 @@
+package com.sparcs.teamf.domain.page;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PageMemberAnswerRepository extends CrudRepository<PageMemberAnswer, Long> {
+
+}
