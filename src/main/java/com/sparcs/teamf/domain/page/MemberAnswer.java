79fcb92daeba4165131bb8c62dcac0ecc044160e
sa46lll <azure394@gmail.com>
2023-03-16 20:35:36 +0900

    refactor: 임포트문 삭제

--- src/main/java/com/sparcs/teamf/domain/page/MemberAnswer.java
+++ src/main/java/com/sparcs/teamf/domain/page/MemberAnswer.java
@@ -2,11 +2,9 @@ package com.sparcs.teamf.domain.page;
 
 import com.sparcs.teamf.domain.BaseEntity;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
