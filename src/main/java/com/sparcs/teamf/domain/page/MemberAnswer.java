a4a264d7c33576ba088693dff670156bdfc9df60
sa46lll <9812smh@naver.com>
2023-03-19 15:13:13 +0900

    feat: 엔티티 컬럼 속성 변경
    issue: #41

--- src/main/java/com/sparcs/teamf/domain/page/MemberAnswer.java
+++ src/main/java/com/sparcs/teamf/domain/page/MemberAnswer.java
@@ -1,10 +1,14 @@
 package com.sparcs.teamf.domain.page;
 
 import com.sparcs.teamf.domain.BaseEntity;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +21,10 @@ public class MemberAnswer extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "page_question_id")
+    private PageQuestion pageQuestion;
 
     private String memberAnswer;
 }
