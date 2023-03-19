93a947d2aefc310088d0b2fb59fdd318a37ef4e7
sa46lll <9812smh@naver.com>
2023-03-19 14:33:37 +0900

    feat: swagger api response 추가
    issue: #35

--- src/main/java/com/sparcs/teamf/api/question/controller/PageQuestionController.java
+++ src/main/java/com/sparcs/teamf/api/question/controller/PageQuestionController.java
@@ -4,8 +4,13 @@ import com.sparcs.teamf.api.auth.dto.EffectiveMember;
 import com.sparcs.teamf.api.question.dto.QuestionsResponse;
 import com.sparcs.teamf.api.question.service.PageQuestionService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,12 +28,26 @@ public class PageQuestionController {
 
     @GetMapping("/questions")
     @Operation(summary = "기본 질문 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = QuestionsResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "unauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found", content = @Content)})
     public QuestionsResponse getPageBasicQuestions(@RequestParam(value = "midCategoryId") long midCategoryId,
                                                    @AuthenticationPrincipal EffectiveMember member) {
         return pageQuestionService.getPageBasicQuestion(midCategoryId, member.getMemberId());
     }
 
     @GetMapping("/{pageId}/questions")
+    @Operation(summary = "기본, 꼬리 질문 목록 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = QuestionsResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
+            @ApiResponse(responseCode = "401", description = "unauthorized", content = @Content),
+            @ApiResponse(responseCode = "403", description = "forbidden", content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found", content = @Content)})
     public QuestionsResponse getPageTailQuestion(@PathVariable("pageId") long pageId,
                                                  @AuthenticationPrincipal EffectiveMember member) {
         return pageQuestionService.getPageTailQuestion(member.getMemberId(), pageId);
