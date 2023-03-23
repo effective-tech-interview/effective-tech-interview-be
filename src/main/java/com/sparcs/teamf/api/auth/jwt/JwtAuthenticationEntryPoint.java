2f256e4859fae7c84850baf9018060252638e3df
Be-student <presentsong@naver.com>
2023-03-23 21:20:00 +0900

    feat: 401 에러 발생시, body 추가

--- src/main/java/com/sparcs/teamf/api/auth/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/sparcs/teamf/api/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -1,11 +1,13 @@
 package com.sparcs.teamf.api.auth.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparcs.teamf.api.error.dto.ErrorResponseDto;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -17,10 +19,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+            AuthenticationException authException) throws IOException, ServletException {
         log.info("인증 정보가 유효하지 않습니다.");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json;charset=UTF-8");
+        String result = objectMapper.writeValueAsString(
+                new ErrorResponseDto(HttpStatus.UNAUTHORIZED.value(), "인증 정보가 유효하지 않습니다."));
+        response.getWriter().write(result);
     }
 }
