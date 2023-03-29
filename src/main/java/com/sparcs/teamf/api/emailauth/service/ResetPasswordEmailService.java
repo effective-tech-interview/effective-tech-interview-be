e6416a02bfaf423826c3349c8b6bf7b4114a0aa5
sa46lll <azure394@gmail.com>
2023-03-29 15:30:22 +0900

    build: 빌드 오류 해결

--- src/main/java/com/sparcs/teamf/api/emailauth/service/ResetPasswordEmailService.java
+++ src/main/java/com/sparcs/teamf/api/emailauth/service/ResetPasswordEmailService.java
@@ -113,6 +113,6 @@ public class ResetPasswordEmailService implements EmailService {
                 + "    </table>\n"
                 + "</body>\n"
                 + "\n"
-                + "</html>";
+                + "</html>").toString();
     }
 }
