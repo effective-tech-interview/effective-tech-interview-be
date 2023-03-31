01642ad5a046dadec0428a1f3b29422c774edafb
Be-student <presentsong@naver.com>
2023-03-31 22:29:52 +0900

    chore: 비밀번호 특수문제 제외 메시지 추가

--- src/main/java/com/sparcs/teamf/api/auth/dto/ResetPasswordEmailRequest.java
+++ src/main/java/com/sparcs/teamf/api/auth/dto/ResetPasswordEmailRequest.java
@@ -12,6 +12,7 @@ public record ResetPasswordEmailRequest(
 
         @NotBlank(message = "올바른 비밀번호를 입력해주세요.")
         @Length(min = 8, max = 20, message = "비밀번호는 영문, 숫자 조합 8자 이상 20자 이하입니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "특수문자는 사용할 수 없습니다")
         @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "비밀번호는 영문, 숫자 조합 8자 이상 20자 이하입니다.")
         String password,
 
