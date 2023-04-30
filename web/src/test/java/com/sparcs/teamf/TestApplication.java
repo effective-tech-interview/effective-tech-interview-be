a86c9f70d99db472c16f7dffbd46be374db5b6e0
Be-student <presentsong@naver.com>
2023-04-30 20:01:29 +0900

    fix: 테스트 작동하도록 수정

--- web/src/test/java/com/sparcs/teamf/TestApplication.java
+++ web/src/test/java/com/sparcs/teamf/TestApplication.java
@@ -2,12 +2,13 @@ package com.sparcs.teamf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = TeamfApplication.class)
+@TestPropertySource(locations = "/application.yml")
+@SpringBootTest
 class TestApplication {
 
     @Test
     void contextLoads() {
     }
-
 }
