92f097522b44f7f053288c6d9cbd020c9f27a360
Be-student <presentsong@naver.com>
2023-05-07 19:26:05 +0900

    chore: 사용되지 않는 testPropertySource 제거

--- web/src/test/java/com/sparcs/teamf/TestApplication.java
+++ web/src/test/java/com/sparcs/teamf/TestApplication.java
@@ -2,9 +2,7 @@ package com.sparcs.teamf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(locations = "/application.yml")
 @SpringBootTest
 class TestApplication {
 
