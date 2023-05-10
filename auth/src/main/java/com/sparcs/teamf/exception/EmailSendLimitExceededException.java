c540f0f6ed57c380c20694eb9c76fa081893e7a6
sa46lll <azure394@gmail.com>
2023-05-10 15:07:11 +0900

    feat: 예외 핸들러 추가

--- auth/src/main/java/com/sparcs/teamf/exception/EmailSendLimitExceededException.java
+++ auth/src/main/java/com/sparcs/teamf/exception/EmailSendLimitExceededException.java
@@ -2,10 +2,4 @@ package com.sparcs.teamf.exception;
 
 public class EmailSendLimitExceededException extends RuntimeException {
 
-    /*
-    public EmailSendLimitExceededException() {
-        super(HttpStatus.TOO_MANY_REQUESTS, "Email sending limit exceeded. Please try again in an hour.");
-
-    }
-     */
 }
--- auth/src/main/java/com/sparcs/teamf/exception/EmailVerificationNotFoundException.java
+++ auth/src/main/java/com/sparcs/teamf/exception/EmailVerificationNotFoundException.java
@@ -2,9 +2,4 @@ package com.sparcs.teamf.exception;
 
 public class EmailVerificationNotFoundException extends RuntimeException {
 
-    /*
-    public EmailVerificationNotFoundException() {
-        super(HttpStatus.BAD_REQUEST, "email request required before verifying your verification code.");
-    }
-     */
 }
