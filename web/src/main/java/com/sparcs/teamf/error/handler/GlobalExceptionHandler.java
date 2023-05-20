551b358380feb8dffe40e1dc9983f17cbc88b1a7
Be-student <presentsong@naver.com>
2023-05-20 19:58:43 +0900

    feat: 쿠키 예외 401 주도록 변경

--- web/src/main/java/com/sparcs/teamf/error/handler/GlobalExceptionHandler.java
+++ web/src/main/java/com/sparcs/teamf/error/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,25 +20,25 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MissingRequestHeaderException.class)
     public ResponseEntity<ErrorResponseDto> onMissingRequestHeader(MissingRequestHeaderException e) {
         return ResponseEntity.badRequest()
-                .body(new ErrorResponseDto(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+            .body(new ErrorResponseDto(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponseDto> onHttpMessageNotReadable(HttpMessageNotReadableException e) {
         if (e.getCause() instanceof MismatchedInputException mismatchedInputException) {
             return ResponseEntity.badRequest()
-                    .body(new ErrorResponseDto(
-                            HttpStatus.BAD_REQUEST.value(),
-                            mismatchedInputException.getPath().get(0).getFieldName() + " 필드의 값이 잘못되었습니다."));
+                .body(new ErrorResponseDto(
+                    HttpStatus.BAD_REQUEST.value(),
+                    mismatchedInputException.getPath().get(0).getFieldName() + " 필드의 값이 잘못되었습니다."));
         }
         return ResponseEntity.badRequest()
-                .body(new ErrorResponseDto(HttpStatus.BAD_REQUEST.value(), "확인할 수 없는 형태의 데이터가 들어왔습니다"));
+            .body(new ErrorResponseDto(HttpStatus.BAD_REQUEST.value(), "확인할 수 없는 형태의 데이터가 들어왔습니다"));
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<ErrorResponseDto> onHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e) {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
-                .body(new ErrorResponseDto(HttpStatus.METHOD_NOT_ALLOWED.value(), "지원하지 않는 HTTP 메서드입니다."));
+            .body(new ErrorResponseDto(HttpStatus.METHOD_NOT_ALLOWED.value(), "지원하지 않는 HTTP 메서드입니다."));
     }
 
     @ExceptionHandler(Exception.class)
@@ -48,10 +49,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponseDto> methodArgumentNotValidExceptionHandle(
-            MethodArgumentNotValidException e) {
+        MethodArgumentNotValidException e) {
         log.error("methodArgumentNotValidException : {}", e);
         return ResponseEntity.badRequest()
-                .body(new ErrorResponseDto(HttpStatus.BAD_REQUEST.value(),
-                        e.getFieldErrors().get(0).getDefaultMessage()));
+            .body(new ErrorResponseDto(HttpStatus.BAD_REQUEST.value(),
+                e.getFieldErrors().get(0).getDefaultMessage()));
+    }
+
+    @ExceptionHandler(MissingRequestCookieException.class)
+    protected ResponseEntity<ErrorResponseDto> missingRequestCookieExceptionHandle(
+        MissingRequestCookieException e) {
+        log.error("missingRequestCookieException : {}", e);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+            .body(new ErrorResponseDto(HttpStatus.UNAUTHORIZED.value(), e.getMessage()));
     }
 }
