c0746202739c3b04170e32f539cbb644ac5aaeae
Be-student <presentsong@naver.com>
2023-05-27 03:50:29 +0900

    feat: kakao oauth client 추가

--- auth/src/main/java/com/sparcs/teamf/oauth2/kakao/KakaoOauth2Client.java
+++ auth/src/main/java/com/sparcs/teamf/oauth2/kakao/KakaoOauth2Client.java
@@ -1,5 +1,7 @@
-package com.sparcs.teamf.oauth2;
+package com.sparcs.teamf.oauth2.kakao;
 
+import com.sparcs.teamf.member.ProviderType;
+import com.sparcs.teamf.oauth2.Oauth2Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,20 +12,26 @@ public class KakaoOauth2Client {
     private final String clientSecret;
     private final String scope;
     private final String redirectUri;
-    private final String authorizeUri;
+    private final String host;
+    private final String authorizationPath;
+    private final String tokenPath;
 
     public KakaoOauth2Client(
         @Value("${spring.security.oauth2.client.registration.kakao.client-id}") String clientId,
         @Value("${spring.security.oauth2.client.registration.kakao.client-secret}") String clientSecret,
         @Value("${spring.security.oauth2.client.registration.kakao.scope}") String scope,
         @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}") String redirectUri,
-        @Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}") String authorizeUri
+        @Value("${spring.security.oauth2.client.provider.kakao.authorization-path}") String authorizationPath,
+        @Value("${spring.security.oauth2.client.provider.kakao.host}") String host,
+        @Value("${spring.security.oauth2.client.provider.kakao.token-path}") String tokenPath
     ) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.scope = scope;
         this.redirectUri = redirectUri;
-        this.authorizeUri = authorizeUri;
+        this.host = host;
+        this.authorizationPath = authorizationPath;
+        this.tokenPath = tokenPath;
     }
 
     public Oauth2Client getClient() {
@@ -32,7 +40,11 @@ public class KakaoOauth2Client {
             .clientSecret(clientSecret)
             .redirectUri(redirectUri)
             .scope(scope)
-            .authorizeUri(authorizeUri)
+            .host(host)
+            .authorizationPath(authorizationPath)
+            .tokenPath(tokenPath)
+            .type(ProviderType.KAKAO)
+            .providerToken(KakaoProviderToken.class)
             .build();
     }
 }
--- auth/src/test/java/com/sparcs/teamf/oauth2/KakaoOauth2ClientTest.java
+++ auth/src/test/java/com/sparcs/teamf/oauth2/KakaoOauth2ClientTest.java
@@ -3,6 +3,9 @@ package com.sparcs.teamf.oauth2;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.sparcs.teamf.member.ProviderType;
+import com.sparcs.teamf.oauth2.kakao.KakaoOauth2Client;
+import com.sparcs.teamf.oauth2.kakao.KakaoProviderToken;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -15,7 +18,7 @@ class KakaoOauth2ClientTest {
     @Test
     void 정상적으로_생성할_수_있다() {
         KakaoOauth2Client kakaoOauth2Client = new KakaoOauth2Client("clientId", "clientSecret", "scope", "redirectUri",
-            "authorizeUri");
+            "authorizationPath", "host", "tokenPath");
         Oauth2Client oauth2Client = kakaoOauth2Client.getClient();
 
         assertSoftly(
@@ -25,7 +28,11 @@ class KakaoOauth2ClientTest {
                 softly.assertThat(oauth2Client.clientSecret()).isEqualTo("clientSecret");
                 softly.assertThat(oauth2Client.scope()).isEqualTo("scope");
                 softly.assertThat(oauth2Client.redirectUri()).isEqualTo("redirectUri");
-                softly.assertThat(oauth2Client.authorizeUri()).isEqualTo("authorizeUri");
+                softly.assertThat(oauth2Client.authorizationPath()).isEqualTo("authorizationPath");
+                softly.assertThat(oauth2Client.host()).isEqualTo("host");
+                softly.assertThat(oauth2Client.tokenPath()).isEqualTo("tokenPath");
+                softly.assertThat(oauth2Client.type()).isEqualTo(ProviderType.KAKAO);
+                softly.assertThat(oauth2Client.providerToken()).isEqualTo(KakaoProviderToken.class);
             }
         );
     }
