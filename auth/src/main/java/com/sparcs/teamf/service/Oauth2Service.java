2c26a220b7c5c50a2dddaeb10808b2b9b8a8cf1e
Be-student <presentsong@naver.com>
2023-05-27 04:05:59 +0900

    feat: oauth 기능 구현

--- auth/src/main/java/com/sparcs/teamf/service/Oauth2Service.java
+++ auth/src/main/java/com/sparcs/teamf/service/Oauth2Service.java
@@ -1,20 +1,32 @@
 package com.sparcs.teamf.service;
 
+import com.sparcs.teamf.dto.TokenResponse;
+import com.sparcs.teamf.jwt.TokenProvider;
+import com.sparcs.teamf.oauth2.EffectiveProfile;
 import com.sparcs.teamf.oauth2.Oauth2Client;
+import com.sparcs.teamf.oauth2.OauthRequester;
+import com.sparcs.teamf.oauth2.ProviderProfile;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 
+@RequiredArgsConstructor
 @Service
 public class Oauth2Service {
 
+    private final SignupService signupService;
+    private final TokenProvider tokenProvider;
+    private final OauthRequester oauthRequester;
+
+
     public String getRedirectUri(Oauth2Client oauth2Client, String requestUri) {
-        return UriComponentsBuilder.newInstance()
-            .scheme("https")
-            .host(oauth2Client.authorizeUri())
-            .queryParam("client_id", oauth2Client.clientId())
-            .queryParam("redirect_uri", requestUri + "/redirect")
-            .queryParam("response_type", "code")
-            .queryParam("scope", oauth2Client.scope())
-            .build(true).toString();
+        return oauth2Client.getProviderRedirectUri(requestUri);
+    }
+
+    public TokenResponse login(Oauth2Client client, String code, String requestUri) {
+        ProviderProfile providerProfile = oauthRequester.requestProfile(client, code, requestUri);
+        //todo logout 을 위한 토큰 관리 기능 추가
+        EffectiveProfile effectiveProfile = signupService.registerAndLogin(providerProfile.providerId(),
+            providerProfile.provider().name());
+        return tokenProvider.createToken(effectiveProfile.memberId());
     }
 }
--- auth/src/test/java/com/sparcs/teamf/service/Oauth2ServiceTest.java
+++ auth/src/test/java/com/sparcs/teamf/service/Oauth2ServiceTest.java
@@ -4,14 +4,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sparcs.teamf.oauth2.Oauth2Client;
 import com.sparcs.teamf.oauth2.Oauth2ClientFixture;
+import com.sparcs.teamf.oauth2.OauthRequester;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class Oauth2ServiceTest {
 
-    private final Oauth2Service oauth2Service = new Oauth2Service();
+    @InjectMocks
+    private Oauth2Service oauth2Service;
+
+    @Mock
+    private OauthRequester oauthRequester;
 
     @Test
     void 정상적으로_리다이렉트_URI_를_생성한다() {
@@ -20,6 +30,6 @@ class Oauth2ServiceTest {
         String result = oauth2Service.getRedirectUri(oauth2Client, "http://localhost:8080");
 
         assertThat(result).isEqualTo(
-            "https://authorizeUri?client_id=clientId&redirect_uri=http://localhost:8080/redirect&response_type=code&scope=scope");
+            "https://host/authorizationPath?client_id=clientId&redirect_uri=http://localhost:8080&response_type=code&scope=scope");
     }
 }
