a6a3117c522bd42fc91849f4b6dad2b977e7ee0a
Be-student <presentsong@naver.com>
2023-05-27 03:54:35 +0900

    test: tokenUtil 테스트 작성

--- auth/src/test/java/com/sparcs/teamf/jwt/TokenUtilTest.java
+++ auth/src/test/java/com/sparcs/teamf/jwt/TokenUtilTest.java
@@ -1,11 +1,14 @@
 package com.sparcs.teamf.jwt;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import io.jsonwebtoken.Claims;
 import java.time.Duration;
 import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 @SuppressWarnings("NonAsciiCharacters")
 class TokenUtilTest {
@@ -62,5 +65,30 @@ class TokenUtilTest {
             softly.assertThat(resultClaim.get("memberId")).isEqualTo(1);
             softly.assertThat(resultClaim.get("name")).isEqualTo("name");
         });
+    }
+
+    @Test
+    void 리프레시_토큰을_쿠키에_잘_설정한다() {
+        HttpServletResponse response = new MockHttpServletResponse();
+        tokenUtil.setRefreshTokenInCookie(response, "refreshToken");
+
+        String result = response.getHeader("Set-Cookie");
+        assertSoftly(softly -> {
+            softly.assertThat(result).contains("refreshToken=refreshToken");
+            softly.assertThat(result).contains("Path=/");
+            softly.assertThat(result).contains("HttpOnly");
+            softly.assertThat(result).contains("Secure");
+            softly.assertThat(result).contains("SameSite=None");
+            softly.assertThat(result).contains("Max-Age=10000");
+        });
+    }
+
+    @Test
+    void id_토큰에서_id_를_잘_가져온다() {
+        String token = "eyJraWQiOiI5ZjI1MmRhZGQ1ZjIzM2Y5M2QyZmE1MjhkMTJmZWEiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI2NzhhODAyMWQwNDkzZjZmMTE1ZTg4MTk4ZGJkYzA0NSIsInN1YiI6IjI4MDE1MDM3NDUiLCJhdXRoX3RpbWUiOjE2ODUxMTcyNjksImlzcyI6Imh0dHBzOi8va2F1dGgua2FrYW8uY29tIiwibmlja25hbWUiOiLshqHsnYDsmrAiLCJleHAiOjE2ODUxMzg4NjksImlhdCI6MTY4NTExNzI2OX0.FErq4mvTOUuO-U6wap8aOwd36Df7AjmVCxoMQvuxOaKls2g6nsWeKpg5YJw-ii48z3hs_FoQ3H16KzXG4WGtV5NjhYLxtBkMT1S6oHnOH0WS5e-EPSsJNMTxol50spCGWvX3JgEwfrkiSuqeb1ZKI8zIa4hPc3c_79ZIz7HXn0wux1paG1-cnecjt5Bsc9-NiVW4Ki0hOno-Axyy3_O-KDFuOYQPj9P_OKY5AGa0sPzCMx1PPBQcOsuub2Ov9BTbWXORuju5tvVwsRYjRgle4cXp_FnJcGWpzSFXubSw1l9YEZ20i4FDVGEnZ92UoqHNP5Osatt-V-86PhORVKuqow";
+
+        String result = tokenUtil.getProviderId(token);
+
+        assertThat(result).isEqualTo("2801503745");
     }
 }
