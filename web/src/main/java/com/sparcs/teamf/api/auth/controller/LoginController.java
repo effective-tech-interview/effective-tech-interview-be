d68f8563df5a13fa6037059be10dc9342e428490
be-student <presentsong@naver.com>
2023-06-21 14:02:03 +0900

    fix: 잘못된 uri경로 수정

--- web/src/main/java/com/sparcs/teamf/api/auth/controller/LoginController.java
+++ web/src/main/java/com/sparcs/teamf/api/auth/controller/LoginController.java
@@ -1,10 +1,6 @@
 package com.sparcs.teamf.api.auth.controller;
 
-import com.sparcs.teamf.api.auth.dto.AccessTokenResponse;
-import com.sparcs.teamf.api.auth.dto.AuthenticateEmailRequest;
-import com.sparcs.teamf.api.auth.dto.LoginRequest;
-import com.sparcs.teamf.api.auth.dto.ResetPasswordEmailRequest;
-import com.sparcs.teamf.api.auth.dto.SendEmailRequest;
+import com.sparcs.teamf.api.auth.dto.*;
 import com.sparcs.teamf.dto.OneTimeTokenResponse;
 import com.sparcs.teamf.dto.TokenResponse;
 import com.sparcs.teamf.jwt.TokenUtil;
@@ -18,20 +14,17 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
 @Tag(name = "Login")
-@RequestMapping("v1/members")
+@RequestMapping("v1/auth")
 public class LoginController {
 
     private final MemberService memberService;
@@ -42,12 +35,12 @@ public class LoginController {
     @PostMapping("/password-reset")
     @Operation(summary = "비밀번호 재설정")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful operation"),
-        @ApiResponse(responseCode = "500", description = "internal server error"),
-        @ApiResponse(responseCode = "400", description = "bad request"),
-        @ApiResponse(responseCode = "401", description = "invalid one time token"),
-        @ApiResponse(responseCode = "403", description = "forbidden"),
-        @ApiResponse(responseCode = "404", description = "not found")})
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+            @ApiResponse(responseCode = "400", description = "bad request"),
+            @ApiResponse(responseCode = "401", description = "invalid one time token"),
+            @ApiResponse(responseCode = "403", description = "forbidden"),
+            @ApiResponse(responseCode = "404", description = "not found")})
     @SecurityRequirements
     public void resetPassword(@RequestBody @Valid ResetPasswordEmailRequest request) {
         memberService.resetPassword(request.email(), request.password(), request.confirmPassword());
@@ -56,11 +49,11 @@ public class LoginController {
     @PostMapping("login")
     @Operation(summary = "로그인")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful operation", content = {
-            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AccessTokenResponse.class))}),
-        @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
-        @ApiResponse(responseCode = "400", description = "bad request", content = @Content),
-        @ApiResponse(responseCode = "404", description = "not found", content = @Content)})
+            @ApiResponse(responseCode = "200", description = "successful operation", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AccessTokenResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
+            @ApiResponse(responseCode = "400", description = "bad request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found", content = @Content)})
     public AccessTokenResponse login(@RequestBody @Valid LoginRequest request,
                                      HttpServletResponse httpServletResponse) {
         TokenResponse tokenResponse = authService.login(request.email(), request.password());
@@ -71,12 +64,12 @@ public class LoginController {
     @PostMapping("/refresh")
     @Operation(summary = "엑세스 토큰 재발급")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful operation", content = {
-            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AccessTokenResponse.class))}),
-        @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
-        @ApiResponse(responseCode = "400", description = "bad request", content = @Content),
-        @ApiResponse(responseCode = "401", description = "invalid refresh token", content = @Content),
-        @ApiResponse(responseCode = "404", description = "not found", content = @Content)})
+            @ApiResponse(responseCode = "200", description = "successful operation", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AccessTokenResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
+            @ApiResponse(responseCode = "400", description = "bad request", content = @Content),
+            @ApiResponse(responseCode = "401", description = "invalid refresh token", content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found", content = @Content)})
     public AccessTokenResponse refresh(@CookieValue("refreshToken") String refreshToken,
                                        HttpServletResponse httpServletResponse) {
         TokenResponse tokenResponse = authService.refresh(refreshToken);
@@ -87,11 +80,11 @@ public class LoginController {
     @PostMapping("/password-reset/email")
     @Operation(summary = "비밀번호 재설정 인증 코드 전송")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful operation"),
-        @ApiResponse(responseCode = "500", description = "internal server error"),
-        @ApiResponse(responseCode = "400", description = "bad request"),
-        @ApiResponse(responseCode = "404", description = "not found"),
-        @ApiResponse(responseCode = "429", description = "too many requests")})
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+            @ApiResponse(responseCode = "400", description = "bad request"),
+            @ApiResponse(responseCode = "404", description = "not found"),
+            @ApiResponse(responseCode = "429", description = "too many requests")})
     public void sendPasswordResetCode(@RequestBody @Valid SendEmailRequest request) {
         emailAuthService.sendPasswordResetCode(request.email());
     }
@@ -99,11 +92,11 @@ public class LoginController {
     @PostMapping("/password-reset/email-verification")
     @Operation(summary = "비밀번호 재설정 인증 코드 검증")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful operation", content = {
-            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = OneTimeTokenResponse.class))}),
-        @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
-        @ApiResponse(responseCode = "400", description = "bad request", content = @Content),
-        @ApiResponse(responseCode = "404", description = "not found", content = @Content)})
+            @ApiResponse(responseCode = "200", description = "successful operation", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = OneTimeTokenResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "internal server error", content = @Content),
+            @ApiResponse(responseCode = "400", description = "bad request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "not found", content = @Content)})
     public OneTimeTokenResponse verifyPasswordResetCode(@RequestBody @Valid AuthenticateEmailRequest request) {
         return emailAuthService.verifyPasswordResetCode(request.email(), request.verificationCode());
     }
--- web/src/main/java/com/sparcs/teamf/api/auth/controller/SignUpController.java
+++ web/src/main/java/com/sparcs/teamf/api/auth/controller/SignUpController.java
@@ -1,10 +1,6 @@
 package com.sparcs.teamf.api.auth.controller;
 
-import com.sparcs.teamf.api.auth.dto.AccessTokenResponse;
-import com.sparcs.teamf.api.auth.dto.AuthenticateEmailRequest;
-import com.sparcs.teamf.api.auth.dto.Oauth2CodeRequest;
-import com.sparcs.teamf.api.auth.dto.SendEmailRequest;
-import com.sparcs.teamf.api.auth.dto.SignupRequest;
+import com.sparcs.teamf.api.auth.dto.*;
 import com.sparcs.teamf.dto.TokenResponse;
 import com.sparcs.teamf.jwt.TokenUtil;
 import com.sparcs.teamf.oauth2.kakao.KakaoOauth2Client;
@@ -16,20 +12,17 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
 
 @Tag(name = "Signup")
 @RestController
-@RequestMapping("v1/signup")
+@RequestMapping("v1")
 @RequiredArgsConstructor
 @SecurityRequirements
 public class SignUpController {
@@ -40,55 +33,55 @@ public class SignUpController {
     private final EmailAuthService emailAuthService;
     private final TokenUtil tokenUtil;
 
-    @PostMapping
+    @PostMapping("/signup")
     @Operation(summary = "회원 가입")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful operation"),
-        @ApiResponse(responseCode = "500", description = "internal server error"),
-        @ApiResponse(responseCode = "400", description = "bad request"),
-        @ApiResponse(responseCode = "403", description = "forbidden"),
-        @ApiResponse(responseCode = "409", description = "the email is already registered")})
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+            @ApiResponse(responseCode = "400", description = "bad request"),
+            @ApiResponse(responseCode = "403", description = "forbidden"),
+            @ApiResponse(responseCode = "409", description = "the email is already registered")})
     public void signup(@RequestBody @Valid SignupRequest request) {
         signupService.signup(request.email(), request.password(), request.confirmPassword());
     }
 
-    @GetMapping("/oauth2/kakao")
+    @GetMapping("/signup/oauth2/kakao")
     @Operation(summary = "카카오 로그인")
     public void kakaoRedirect(HttpServletResponse response, String redirectUri)
-        throws IOException {
+            throws IOException {
         String result = oauth2Service.getRedirectUri(kakaoOauth2Client.getClient(), redirectUri);
         response.sendRedirect(result);
     }
 
-    @PostMapping("/oauth2/kakao/code")
+    @PostMapping("/signup/oauth2/kakao/code")
     @Operation(summary = "카카오 로그인 인가 코드를 받아서, 액세스 토큰을 발급해준다")
     public ResponseEntity<AccessTokenResponse> kakaoLogin(@RequestBody @Valid Oauth2CodeRequest oauth2CodeRequest,
                                                           HttpServletResponse response) {
         TokenResponse tokenResponse = oauth2Service.login(kakaoOauth2Client.getClient(), oauth2CodeRequest.code(),
-            oauth2CodeRequest.redirectUri());
+                oauth2CodeRequest.redirectUri());
         tokenUtil.setRefreshTokenInCookie(response, tokenResponse.refreshToken());
         return ResponseEntity.ok(new AccessTokenResponse(tokenResponse.memberId(), tokenResponse.accessToken()));
     }
 
-    @PostMapping("/email/send")
+    @PostMapping("/auth/email/send")
     @Operation(summary = "회원가입 이메일 인증 코드 전송")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful operation"),
-        @ApiResponse(responseCode = "500", description = "internal server error"),
-        @ApiResponse(responseCode = "400", description = "bad request"),
-        @ApiResponse(responseCode = "409", description = "the email is already registered"),
-        @ApiResponse(responseCode = "429", description = "too many requests")})
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+            @ApiResponse(responseCode = "400", description = "bad request"),
+            @ApiResponse(responseCode = "409", description = "the email is already registered"),
+            @ApiResponse(responseCode = "429", description = "too many requests")})
     public void sendEmailForSignup(@RequestBody @Valid SendEmailRequest request) {
         emailAuthService.sendEmailForSignup(request.email());
     }
 
-    @PostMapping("/email/authenticate")
+    @PostMapping("/auth/email/authenticate")
     @Operation(summary = "회원가입 이메일 인증 코드 확인")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "successful operation"),
-        @ApiResponse(responseCode = "500", description = "internal server error"),
-        @ApiResponse(responseCode = "400", description = "bad request"),
-        @ApiResponse(responseCode = "422", description = "invalid verification code")})
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "500", description = "internal server error"),
+            @ApiResponse(responseCode = "400", description = "bad request"),
+            @ApiResponse(responseCode = "422", description = "invalid verification code")})
     public void authenticateEmailForSignup(@RequestBody @Valid AuthenticateEmailRequest request) {
         emailAuthService.authenticateEmailForSignup(request.email(), request.verificationCode());
     }
